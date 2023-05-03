5cdef5f1a78f7ff1f1be504cd66ea7eaceaa3e50
paterane <peteroomm@gmail.com>
2023-05-03 17:04:46 +0630

    updated function|compare linear and bst search

--- main.c
+++ main.c
@@ -6,18 +6,6 @@ typedef struct node{
     struct node *left;
     struct node *right;
 }Node;
-
-Node *rootNode = NULL;  // empty tree declaration
-#define SIZE 16         // size of data in the array
-int data_arr[SIZE] = {0}; 
-/**
- * Data filling in the array in ascending order
-*/
-void init_data(){
-    for(int i=0; i<SIZE; i++){
-        data_arr[i] = i+1;
-    }
-}
 /**
  * return nearest value of float
 */
@@ -64,26 +52,78 @@ void printAllData(Node *rootNode){
     
 }
 /**
- * building bst tree with sorted data
+ * Arranging data for bst tree
 */
-void build_tree(int start, int max, int d_size){ // at frist start = 2 // max = 3
-    int mid = div_2_round((float)start + (float)(max - start)/2); //mid 3
-    if(mid != max){ // equal
-        insertNode(&rootNode, data_arr[mid-1]);
-        build_tree(mid, max, d_size); //
+void data_arrange(int start, int max, Node **rootNode, int *arr){ // at frist start = 0, max = size of data set
+    int mid = div_2_round((float)start + (float)(max - start)/2); 
+    if(mid != max){ 
+        insertNode(&(*rootNode), arr[mid-1]);
+        data_arrange(mid, max, &(*rootNode), arr); 
         max = mid;
-        build_tree(start, max, d_size);
-    }
-    if(mid == d_size){
-        insertNode(&rootNode, data_arr[mid-1]);
+        data_arrange(start, max, &(*rootNode), arr);
     }
 }
+/**
+ * build bst tree with the given data set
+*/
+void build_tree(Node **rootNode, int *arr, int arr_size){
+    data_arrange(0, arr_size, &(*rootNode), arr);
+    insertNode(&(*rootNode), arr[arr_size-1]);
+}
+/**
+ * a structure to return data found or not and number of step it had to go
+*/
+struct SCHINFO {
+    unsigned int found:1; //bit whether data is found or not
+    int step; // number of steps it had to go
+};
+struct SCHINFO search_info;
+/**
+ * Search data from the tree
+*/
+struct SCHINFO search_data(Node *rootNode, int data){
+    if(rootNode != NULL){
+        if(data == rootNode->data){
+            search_info.found = 1;
+        }
+        else if(data > rootNode->data){
+            search_info.step++;
+            search_data(rootNode->right, data);
+        }
+        else{
+            search_info.step++;
+            search_data(rootNode->left, data);
+        }
+    }
+    else{
+        search_info.found = 0;
+    }
+    return search_info;
+}
 
+struct SCHINFO linear_search(int *data_set,int data_size, int data){
+    for(int i=0; i<data_size; i++){
+        if(data == data_set[i]){
+            search_info.found = 1;
+            return search_info;
+        }
+        else{
+            search_info.step++;
+        }
+    }
+    search_info.found = 0;
+    return search_info;
+}
 
 int main(){
-
-    init_data();
-    build_tree(0, SIZE, SIZE);
-
+    Node *rootNode = NULL;
+    int data_set[1000];
+    for(int i=0; i<sizeof(data_set)/sizeof(int); i++){
+        data_set[i] = i+1;
+    }
+    build_tree(&rootNode, data_set, sizeof(data_set)/sizeof(int));
+    struct SCHINFO data_found = search_data(rootNode, 50);
+    // struct SCHINFO data_found = linear_search(data_set, sizeof(data_set)/sizeof(int), 50);
+    printf("Number of steps it went through: %d\nData %s\n", data_found.step, (data_found.found)? "found":"not found");
     return 0;
 }
